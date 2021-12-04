3981abdf8859f9a640f13a9157c0a7c7eed0183f
maryanseg <areyansegura@gmail.com>
2021-12-04 08:05:44 -0500

    listar y eliminiar usuario

--- flyemirates/src/app/modulos/seguridad/login/login.component.ts
+++ flyemirates/src/app/modulos/seguridad/login/login.component.ts
@@ -5,6 +5,7 @@ import * as cryptoJS from 'crypto-js';
 import { SeguridadService } from
 'src/app/servicios/seguridad.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2'	
 
 @Component({
   selector: 'app-login',
@@ -18,10 +19,27 @@ export class LoginComponent implements OnInit {
     private router: Router) { }
 fgValidacion = this.fb.group({
     correo: ['', [Validators.required, Validators.email]],
-    clave: ['', [Validators.required]]
+    clave: ['', [Validators.required, Validators.minLength(6)]]
     });
   ngOnInit(): void {
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   identificarUsuario() {
     let usuario =
     this.fgValidacion.controls["correo"].value;
@@ -32,11 +50,28 @@ fgValidacion = this.fb.group({
     claveCifrada).subscribe(
     (data: any) => {
     this.seguridadService.almacenarSesion(data)
-    this.router.navigate(['/index']);
-    },
+    Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Bienvenido',
+            showConfirmButton: false,
+            timer: 1500
+          }).then(() =>{
+            this.router.navigate(['/index']);
+          })
+        },
     (error: any) => {
-    console.log(error)
-    alert("Datos inválidos");
+          console.log(error)
+          Swal.fire({
+            title: 'Error!',
+            text: 'Datos invalidos',
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+          })
+        }
+        );
+
+      }
     }
     );
     }
