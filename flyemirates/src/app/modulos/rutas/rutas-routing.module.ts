ac4d0126b3f4a5628296ab38eae2cd9bd3df4c4d
DiegoRCasallas <master.drcd@hotmail.com>
2021-12-12 21:46:54 -0500

    Guardianes Aero,rutas

--- flyemirates/src/app/modulos/rutas/rutas-routing.module.ts
+++ flyemirates/src/app/modulos/rutas/rutas-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SessionGuard } from 'src/app/guards/session.guard';
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
 import { GetComponent } from './get/get.component';
@@ -11,9 +12,11 @@ component: CreateComponent,
 },{
 path: 'edit/:id',
 component: EditComponent,
+canActivate: [SessionGuard]
 },{
 path: 'get',
 component: GetComponent,
+canActivate: [SessionGuard]
 },{
 path: '',
 redirectTo: 'get'
